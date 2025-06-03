a66cd03f20c224355981c544e3eeb2c1997f5df5
yumachin <curryuma925@gmail.com>
2025-06-03 16:49:21 +0900

    機能追加: 入力処理付きの基本Todo機能を実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [text, setText] = useState("")
+  const [todos, setTodos] = useState([])
+
+  const handleInput = (e) => {
+    setText(e.target.value)
+  }
+  
+  const handleAdd = () => {
+    if (text.trim() !== "") {
+      setTodos([...todos, text])
+      setText("")
+    }
+  }
+
   return (
     <div className='container'>
       <h1>Todoリスト</h1>
       <div className='input-area'>
         <input
+          value={text}
           placeholder='やることを入力...'
+          onChange={handleInput}
         />
-        <button>追加</button>
+        <button onClick={handleAdd}>追加</button>
       </div>
       <ul>
-        <li>プログラミング演習1課題</li>
-        <li>基礎ゼミ</li>
-        <li>Paiza演習問題</li>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
       </ul>
     </div>
   )
