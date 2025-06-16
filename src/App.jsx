3a0a21b0ec2d695c48aa3f98bf20ab73b8328e63
yumachin <curryuma925@gmail.com>
2025-06-16 14:16:05 +0900

    機能追加: Todoリストに削除機能と入力バリデーションを実装

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,19 @@ function App() {
   const handleInput = (e) => {
     setText(e.target.value)
   }
-  
+
   const handleAdd = () => {
     if (text.trim() !== "") {
       setTodos([...todos, text])
-      setText("")
+    } else {
+      alert("タスクを入力してください")
     }
+    setText("")
+  }
+
+  const handleDelete = (index) => {
+    const newTodos = todos.filter((todo, i) => i !== index)
+    setTodos(newTodos)
   }
 
   return (
@@ -28,8 +35,14 @@ function App() {
         <button onClick={handleAdd}>追加</button>
       </div>
       <ul>
-        {todos.map((todo) => (
-          <li>{todo}</li>
+        {todos.map((todo, index) => (
+          <li>
+            {todo}
+            <div className='buttons'>
+              <button>編集</button>
+              <button className='delete-button' onClick={() => handleDelete(index)}>削除</button>
+            </div>
+          </li>
         ))}
       </ul>
     </div>
